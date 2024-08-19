4ea6c53a2096f21c4b464bf12fbfe92b5af1f0dd
dongyibo <386083722@qq.com>
2024-08-19 15:06:38 +0800

    feat(Propagation) change ticks from global to frame-related

--- src/checker/carChecker.cpp
+++ src/checker/carChecker.cpp
@@ -754,8 +754,8 @@ namespace car
 
         int originalUCsize = uc.size();
 
-        static int prop_tick = 0;
-        ++prop_tick;
+        static std::map<int,int> prop_tick;
+        ++prop_tick[level];
         if(!prop_res)
         {
             auto upcoming_uc = prop_solver -> get_uc();
@@ -815,7 +815,7 @@ namespace car
             }
         }
 
-        CARStats.count_prop_end(!prop_res,strongInductive, originalUCsize, prop_tick);
+        CARStats.count_prop_end(!prop_res,strongInductive, originalUCsize, prop_tick[level]);
     }
 
     bool Checker::convTriggered(int level)
